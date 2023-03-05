d251dc2aa0c20a25c6b7efa3fedbb9ac5d7440f3
Pythogen HP <bubblewrap72@gmail.com>
2023-03-04 23:54:01 -0500

    Updated UI

--- lib/course_edit.dart
+++ lib/course_edit.dart
@@ -34,9 +34,9 @@ class _CourseEditorState extends State<CourseEditor> {
             : "${widget.course.gradeDesired}");
 
     courseName.addListener(() {
-      if (widget.course.currentGrade!=double.negativeInfinity) {
+      if (widget.course.currentGrade != double.negativeInfinity) {
         widget.course.name = courseName.text;
-    }
+      }
     });
   }
 
@@ -53,14 +53,16 @@ class _CourseEditorState extends State<CourseEditor> {
       child: Container(
         margin: EdgeInsets.all(8),
         padding: EdgeInsets.symmetric(vertical: 8, horizontal: 24),
-        decoration: BoxDecoration(borderRadius: BorderRadius.circular(18), border: Border.all(color: Colors.grey, width: .3)),
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(18),
+            border: Border.all(color: Colors.grey, width: .3)),
 
         child: Column(
           children: <Widget>[
             Txt(question),
             Row(children: [
               Expanded(child: Row(children: [
-                Expanded(child: TextField(controller: c, decoration: InputDecoration(hintText: hint),)),
+                Expanded(child: TextField(
+                  controller: c, decoration: InputDecoration(hintText: hint),)),
                 Txt(followText),
               ],)),
               if (showButton) Button(okText, okButton),
@@ -89,7 +91,7 @@ class _CourseEditorState extends State<CourseEditor> {
             "Next",
             widget.course.name.isEmpty, () {
           widget.course.name = courseName.text;
-          print(widget.course.name+ " hello");
+          print(widget.course.name + " hello");
           setState(() {
 
           });
@@ -115,7 +117,7 @@ class _CourseEditorState extends State<CourseEditor> {
         }),
         card(
             "How much of your grade is the exam worth?",
-            widget.course.currentGrade!=double.negativeInfinity,
+            widget.course.currentGrade != double.negativeInfinity,
             examWeight,
             "Exam Weight",
             "%",
@@ -123,7 +125,7 @@ class _CourseEditorState extends State<CourseEditor> {
             widget.course.examWeight == double.negativeInfinity, () {
           double? grade = double.tryParse(currentGrade.text);
           if (grade != null && grade >= 0 && grade <= 100) {
-            widget.course.examWeight = grade*.01;
+            widget.course.examWeight = grade;
             setState(() {
 
             });
@@ -134,47 +136,77 @@ class _CourseEditorState extends State<CourseEditor> {
         }),
         card(
             "What grade do you want overall?",
-            widget.course.examWeight!=double.negativeInfinity,
+            widget.course.examWeight != double.negativeInfinity,
             gradeDesired,
             "Grade Desired",
             "",
             "Calculate",
-            widget.course.gradeDesired == double.negativeInfinity, () {
-          double? grade = double.tryParse(currentGrade.text);
-          if (grade != null && grade >= 0 && grade <= 100) {
-            widget.course.gradeDesired = grade;
-            widget.course.examWeight = double.tryParse(examWeight.text) ?? widget.course.examWeight;
-            if (widget.course.examWeight>1) widget.course.examWeight*=.01;
-            widget.course.currentGrade = double.tryParse(currentGrade.text) ?? widget.course.currentGrade;
-
-            setState(() {
-
-            });
-          } else {
-            ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Txt("Invalid Input")));
-          }
-        }),
-        if (gN!=null)           Container(padding: EdgeInsets.all(24), constraints: BoxConstraints(maxWidth: 250),
+            widget.course.gradeDesired == double.negativeInfinity, calculate),
+        if (gN != null) Container(padding: EdgeInsets.all(24),
+          constraints: BoxConstraints(maxWidth: 250),
           child: ClipOval(child: AspectRatio(aspectRatio: 1, child: Stack(
-      children: [
-          Column(
-            children: <Widget>[
-              Flexible(child: Container(color: Colors.grey[300],), flex: 100-gN.toInt()),
-              Flexible(child: Container(color: widget.course.color), flex: gN.toInt()),
+            children: [
+              Column(
+                children: <Widget>[
+                  Flexible(child: Container(color: Colors.grey[300],),
+                      flex: 100 - gN.toInt()),
+                  Flexible(child: Container(color: widget.course.color),
+                      flex: gN.toInt()),
+
+                ],
+              ),
+              Center(child: Txt(widget.course.allValuesSet ? "${(widget.course
+                  .gradeNeeded).round()}%" : "N/A")),
 
             ],
           ),
-          Center(child: Txt(widget.course.allValuesSet ? "${(widget.course.gradeNeeded).round()}%" : "N/A")),
 
+
+          )),
+        ),
+
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TextButton(onPressed: calculate, child: Txt("Recalculate")),
+            TextButton(onPressed: Get.back, child: Txt("Menu")),
+          ],
+        )
       ],
-    ),
-
-
-
-
-    )),
-        )],
     ),);
+  }
+
+  void calculate() {
+    double? grade = double.tryParse(currentGrade.text);
+    bool invalid = false;
+    if (grade != null && grade >= 0 && grade <= 100) {
+      widget.course.currentGrade = grade;
+    } else {
+      invalid = true;
+    }
+
+    grade = double.tryParse(gradeDesired.text);
+    if (grade != null && grade >= 0 && grade <= 100) {
+      widget.course.gradeDesired = grade;
+    } else {
+      invalid = true;
+    }
+
+    grade = double.tryParse(examWeight.text);
+    if (grade != null && grade >= 0 && grade <= 100) {
+      widget.course.examWeight = grade;
+    } else {
+      invalid = true;
+    }
+
+
+    setState(() {
+
+    });
+    if (invalid) {
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Txt("Invalid Input")));
+    }
   }
 
 }
